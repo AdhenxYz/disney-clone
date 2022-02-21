b4294e900ffdf1e0469ed7140e4b5d311587b0d5
AdhenxYz <77957253+AdhenxYz@users.noreply.github.com>
2022-02-21 16:31:19 +0700

    ADD: Details Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Home from './Components/Home';
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom'
+import DetailsPage from './Components/DetailsPage';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <DetailsPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
